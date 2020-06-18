46faaceb3abc2603de9e07a89b08522643b6052c
vineelavinny <rajulapati.vineela@gmail.com>
2020-06-18 17:50:39 +0530

    Add files via upload

--- storage/framework/views/17b183fa465216ebd5addf2062e65228d6e9048b.php
+++ storage/framework/views/17b183fa465216ebd5addf2062e65228d6e9048b.php
@@ -1,5 +1,7 @@
 <h1>Customers</h1>
 <a href='/customers/create'>Add New Customer</a>
+<a href='/customers?active=1'>Active</a>
+<a href='/customers?active=0'>Inactive</a>
 <?php $__empty_1 = true; $__currentLoopData = $customers; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $c): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
 <p><strong><a href="/customers/<?php echo e($c->id); ?>"><?php echo e($c->name); ?></a></strong>(<?php echo e($c->email); ?>)</p>
 <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
---
+++ storage/framework/views/58e6b26ea9ef2e3996aa4cc89200a576aee1c606.php
@@ -0,0 +1,2 @@
+[<?php echo e($slot); ?>](<?php echo e($url); ?>)
+<?php /**PATH C:\Users\rajul\larproject-1\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
